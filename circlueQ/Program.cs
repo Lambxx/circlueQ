f8c7c7a6749cade06f8e3bdfe814ffa3aa7a76bc
xoxolunaxox <23667944@stu.mmu.ac.uk>
2023-08-29 14:15:09 +0100

    Add project files.

---
+++ circlueQ/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace circlueQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Q {
+        private int head;
+        private int tail;
+        private int MaxqSize;
+        private int []qArr;
+      
+
+        public Q(int size){
+            this.qArr = new int[size];
+            this.head = 0;
+            this.tail = 
+
+
+        }
+    }
+}
