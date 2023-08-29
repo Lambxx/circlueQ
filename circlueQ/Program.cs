359f445e31318a04dc13ccf466364d5f8c3908c1
xoxolunaxox <23667944@stu.mmu.ac.uk>
2023-08-29 14:28:56 +0100

    Enque added

--- circlueQ/Program.cs
+++ circlueQ/Program.cs
@@ -10,22 +10,36 @@ namespace circlueQ
     {
         static void Main(string[] args)
         {
+            Q testq = new Q(10);
+            testq.qEnqueue(0);
         }
     }
 
     class Q {
         private int head;
         private int tail;
-        private int MaxqSize;
         private int []qArr;
       
 
         public Q(int size){
             this.qArr = new int[size];
             this.head = 0;
-            this.tail = 
+            this.tail = 0;
+            Console.WriteLine("new Q of size " + qArr.Length + " created");
+            Console.ReadLine();
 
 
+        }
+        public bool qEnqueue(int val) {
+            if (((head + 1) % (qArr.Length)) == tail)
+            {
+                return false;
+            }
+            else {
+                qArr[head] = val;
+                head = (head + 1) % qArr.Length;
+                return true;
+            }
         }
     }
 }
